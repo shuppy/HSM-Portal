12775b68f06ec898695b31f5e37defa064d44537
deyshupor <hardeyshupor@yahoo.com>
2014-04-12 02:29:49 +0100

    Friendship Group, ChoirSplit, and Reports. Also, changes to views, stored procedures and triggers on tables. Some scalar functions on database as well.

---
+++ HSM/HSM/Controllers/ChoirSplitsController.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HsmBI;
+
+namespace HSM.Controllers
+{
+    public class ChoirSplitsController : Controller
+    {
+        private defaultcon db = new defaultcon();
+        //GET: /ChoirSplits - Splits Management
+       
+        public ActionResult SearchSplits(string ChoirSplit)
+        {
+            var choirlist = getchoirlist(ChoirSplit );
+            return PartialView(choirlist);
+        }
+
+        private List<vwMembersList_General> getchoirlist(string choirsplit)
+        {
+            return  db.vwMembersList_General.Where( c => c.Split.Contains(choirsplit)).ToList();
+            
+        }
+        
+        public ActionResult viewsplit(FormCollection form)
+        {
+            //Get Choir split
+            int split = Convert.ToInt32(form["ChoirSplit_Id"]);
+            //Redirect to url
+            return RedirectToAction("Reshuffle?split=" + split); //this will give error
+        }
+
+        [HttpPost ]
+        public ActionResult viewpart(FormCollection form)
+        {
+            //Get Choir split
+            int part = Convert.ToInt32(form["PartId"]);
+            //Redirect to url
+            return RedirectToAction("Reshuffle?part=" + part);
+        }
+
+        [HttpPost ]
+        public ActionResult viewsplitpart(FormCollection form)
+        {
+            //Get Choir split
+            int split = Convert.ToInt32(form["ChoirSplit_Id"]);
+            //Redirect to url
+            return RedirectToAction("Reshuffle?split=" + split);
+        }
+        [HttpPost]
+        public ActionResult movetosplit(FormCollection form)
+        {
+            // get collection of selected members
+            var selected = form.GetValues("movechk");
+            int choir = Convert.ToInt32(form["Choir"]);
+
+            //Now loop through and assign to choir one
+            foreach (var id in selected )
+            {
+                int i = Convert.ToInt32(id);
+                Members member = db.Members.Find(i);
+                member.ChoirSplit_Id = choir;
+
+                //Save
+                db.Entry(member).State = EntityState.Modified;
+                db.SaveChanges();
+
+                //loop
+            }
+            return RedirectToAction("Reshuffle", "ChoirSplits", new { split = choir });
+        }
+        //public async Task<ActionResult> Reshuffle()
+        //{
+        //    return View(await db.vwMembersList_General.ToListAsync());
+        //}
+
+        
+        public ViewResult Reshuffle(string split ="0",string part ="0")
+        {
+            ViewData["currentSplit"] = split ?? string.Empty;
+            ViewData["currentPart"] = part ?? string.Empty;
+            ViewBag.Splits = new SelectList(db.ChoirSplits.OrderBy(g => g.Description), "SplitId", "Description");
+            ViewBag.Parts = new SelectList(db.ChoirParts.OrderBy(p => p.Part), "sn", "Part");
+            //Check if to list all
+            int npart = string.IsNullOrEmpty(part) ? 0 : Convert.ToInt32(part);
+            int nsplit = String.IsNullOrEmpty(split ) ? 0:  Convert.ToInt32(split);
+            var list = nsplit == 0 ? 
+                npart == 0? db.vwMembersList_General :
+                from v in db.vwMembersList_General 
+                where v.PartId == npart
+                select v:
+                npart == 0?
+                from v in db.vwMembersList_General
+                where v.SplitId == nsplit 
+                select v :
+                from v in db.vwMembersList_General
+                where v.SplitId == nsplit && v.PartId == npart 
+                select v;
+
+            return View(list.ToList());
+        }
+        // GET: /ChoirSplits/
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.ChoirSplits.ToListAsync());
+        }
+
+        // GET: /ChoirSplits/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChoirSplits choirsplits = await db.ChoirSplits.FindAsync(id);
+            if (choirsplits == null)
+            {
+                return HttpNotFound();
+            }
+            return View(choirsplits);
+        }
+
+        // GET: /ChoirSplits/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /ChoirSplits/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="SplitId,Description")] ChoirSplits choirsplits)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ChoirSplits.Add(choirsplits);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(choirsplits);
+        }
+
+        // GET: /ChoirSplits/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChoirSplits choirsplits = await db.ChoirSplits.FindAsync(id);
+            if (choirsplits == null)
+            {
+                return HttpNotFound();
+            }
+            return View(choirsplits);
+        }
+
+        // POST: /ChoirSplits/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="SplitId,Description")] ChoirSplits choirsplits)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(choirsplits).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(choirsplits);
+        }
+
+        // GET: /ChoirSplits/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChoirSplits choirsplits = await db.ChoirSplits.FindAsync(id);
+            if (choirsplits == null)
+            {
+                return HttpNotFound();
+            }
+            return View(choirsplits);
+        }
+
+        // POST: /ChoirSplits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ChoirSplits choirsplits = await db.ChoirSplits.FindAsync(id);
+            db.ChoirSplits.Remove(choirsplits);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /* Methods */
+
+        
+    }
+}
---
+++ HSM/HSM/Controllers/FriendshipGroupsController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HsmBI;
+
+namespace HSM.Controllers
+{
+    public class FriendshipGroupsController : Controller
+    {
+        private defaultcon db = new defaultcon();
+
+        // GET: /FriendshipGroups/
+        public async Task<ActionResult> Index()
+        {
+            
+            return View(await db.FriendshipGroups.ToListAsync());
+        }
+
+        // GET: /FriendshipGroups/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendshipGroups friendshipgroups = await db.FriendshipGroups.FindAsync(id);
+            if (friendshipgroups == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendshipgroups);
+        }
+
+        // GET: /FriendshipGroups/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /FriendshipGroups/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include="ClubId,Code,Description")] FriendshipGroups friendshipgroups)
+        {
+            if (ModelState.IsValid)
+            {
+                db.FriendshipGroups.Add(friendshipgroups);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(friendshipgroups);
+        }
+
+        // GET: /FriendshipGroups/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendshipGroups friendshipgroups = await db.FriendshipGroups.FindAsync(id);
+            if (friendshipgroups == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendshipgroups);
+        }
+
+        // POST: /FriendshipGroups/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include="ClubId,Code,Description")] FriendshipGroups friendshipgroups)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(friendshipgroups).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(friendshipgroups);
+        }
+
+        // GET: /FriendshipGroups/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendshipGroups friendshipgroups = await db.FriendshipGroups.FindAsync(id);
+            if (friendshipgroups == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendshipgroups);
+        }
+
+        // POST: /FriendshipGroups/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            FriendshipGroups friendshipgroups = await db.FriendshipGroups.FindAsync(id);
+            db.FriendshipGroups.Remove(friendshipgroups);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public ViewResult Reshuffle(string club = "0", string member ="0")
+        {
+            
+            ViewBag.Clubs = new SelectList(db.FriendshipGroups.OrderBy(g => g.Description), "ClubId", "Description");
+            ViewBag.Members = new SelectList(db.vwMembersList_General.OrderBy(m =>m.Fullname), "MemberId", "Fullname");
+            //Check if to list all
+            int nclub = string.IsNullOrEmpty(club) ? 0 : Convert.ToInt32(club);
+            int xname = string.IsNullOrEmpty(member) ? 0 : Convert.ToInt32(member);
+            var list = nclub == 0 ?
+                xname == 0 ?
+                from m in db.vwMembersList_General
+                orderby m.Friendship_Group ,m.Fullname 
+                select m :
+                from m in db.vwMembersList_General
+                where m.MemberId == xname
+                orderby m.Friendship_Group, m.Fullname
+                select m :
+                    from m in db.vwMembersList_General
+                    where m.ClubId == nclub
+                    orderby m.Friendship_Group, m.Fullname
+                    select m;
+
+            return View(list.ToList());
+        }
+
+        [HttpPost]
+
+        public ActionResult filterclub( FormCollection form)
+        {
+            //Get the club id
+            int club = Convert.ToInt32(form["clubs"]); //this will always return the value of the element
+            return RedirectToAction("Reshuffle", "FriendshipGroups", new { club = club });
+        }
+
+        [HttpPost ]
+        public ActionResult filtername(FormCollection form)
+        {
+            //Get the club id
+            string name = form["names"].ToString(); //this will always return the value of the element
+            return RedirectToAction("Reshuffle", "FriendshipGroups", new { member = name });
+        }
+
+        [HttpPost]
+        public ActionResult movetoclub(FormCollection form)
+        {
+            // get collection of selected members
+            var selected = form.GetValues("movechk");
+            int club = Convert.ToInt32(form["club"]);
+
+            foreach (var id in selected)
+            {
+                int i = Convert.ToInt32(id);
+                Members member = db.Members.Find(i);
+                member.Friendship_Id = club;
+
+                //Save
+                db.Entry(member).State = EntityState.Modified;
+                db.SaveChanges();
+
+                //loop
+            }
+                
+
+                return RedirectToAction("Reshuffle", "FriendshipGroups", new { club = club });
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
